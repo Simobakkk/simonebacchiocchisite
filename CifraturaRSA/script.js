a0fd65e6c4df5f2d6a73d072aaccc668aa0955fd
Simobakkk <simone.bak2007@gmail.com>
2026-02-04 01:05:42 +0100

    Update
    Last update

--- CifraturaRSA/script.js
+++ CifraturaRSA/script.js
@@ -2,14 +2,17 @@ async function criptRSA(){
     const text = document.getElementById("text").value;
     const textASCII =  conversionASCII(text);
     console.log(textASCII);
+
     const ris = await fetch("chiavi_pubbliche.json"); //chiamata HTTP 
     const chiave_pubblica = await ris.json(); // risposta convertita in JSON
     const e = chiave_pubblica.e;
     const n = chiave_pubblica.n;
+    
     let c = []; //messaggio cifrato
     for(let i=0; i<textASCII.length; i++){
         c.push(Math.pow(textASCII[i], e) % n); 
     }
+
     localStorage.setItem("messaggio_cifrato", JSON.stringify(c));
     window.location.href = "decifraturaRSA.html";
 }
--- CifraturaRSA/script2.js
+++ CifraturaRSA/script2.js
@@ -1,21 +1,26 @@
-async function decriptRSA(){
+async function decriptRSA() {
     const ris = await fetch("chiave_privata.json");
     const chiave_privata = await ris.json();
     const d = chiave_privata.d;
     const n = chiave_privata.n;
+
     const c = JSON.parse(localStorage.getItem("messaggio_cifrato"));
-    let m = []; //messaggio originale 
-    for(let i=0; i<c.length; i++){
+
+    let m = []; //messaggio originale
+
+    for (let i = 0; i < c.length; i++) {
         m.push(Math.pow(parseInt(c[i]), d) % n);
     }
     m = deconversionASCII(m);
-    m = document.getElementById("text").innerHTML;
+
+    // mostra il messaggio nel DOM
+    document.getElementById("text").innerHTML = m;
 }
 
-function deconversionASCII(text){
+function deconversionASCII(text) {
     const vet = [];
-    for(let i=0; i<text.length; i++){
-        vet.push(text.fromCharCode(i));
+    for (let i = 0; i < text.length; i++) {
+        vet.push(String.fromCharCode(text[i]));
     }
-    return vet;
+    return vet.join("");
 }
